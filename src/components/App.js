7efa5ddeca01acaf7a0d8ce3d0cb96b8cf0edad1
JP Erasmus <jperasmus11@gmail.com>
2017-12-21 15:32:39 +0200

    feat: Add placeholder for new post view

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 
 import NotFound from './NotFound';
 import Category from './Category';
+import NewPost from './NewPost';
+import PostDetails from './PostDetails';
 import './App.css';
 
 const { Header, Content, Footer } = Layout;
@@ -15,7 +17,14 @@ class App extends Component {
         <Header className="header">
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }}>
             <Menu.Item key="home">
-              <Link to="/">Readable</Link>
+              <Link to="/">
+                <Icon type="home" />Readable
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="add-post">
+              <Link to="/add-post">
+                <Icon type="plus-square-o" />Add Post
+              </Link>
             </Menu.Item>
           </Menu>
         </Header>
@@ -24,13 +33,20 @@ class App extends Component {
             <Content style={{ padding: '0 24px', minHeight: 280 }}>
               <Switch>
                 <Route exact path="/" component={Category} />
+                <Route exact path="/add-post" component={NewPost} />
                 <Route exact path="/:category" component={Category} />
+                <Route exact path="/:category/:post_id" component={PostDetails} />
                 <Route component={NotFound} />
               </Switch>
             </Content>
           </Layout>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>Readable ©2017 Created by JP Erasmus</Footer>
+        <Footer style={{ textAlign: 'center' }}>
+          Readable © 2017 Created by{' '}
+          <a href="https://jperasmus.me" target="_blank" rel="noopener noreferrer">
+            JP Erasmus
+          </a>
+        </Footer>
       </Layout>
     );
   }
---
+++ src/components/NewPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewPost extends Component {
+  render() {
+    return <div>New Post Placeholder...</div>;
+  }
+}
+
+NewPost.defaultProps = {};
+
+NewPost.propTypes = {};
+
+export default NewPost;
