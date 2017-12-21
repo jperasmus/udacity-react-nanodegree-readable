65cd40c68f1f0136f99906a7d5e070c52a2c562e
JP Erasmus <jperasmus11@gmail.com>
2017-12-21 17:20:26 +0200

    feat: Add `EditPost` placeholder component

---
+++ src/components/EditPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditPost extends Component {
+  render() {
+    return <div>Edit Post Placeholder...</div>;
+  }
+}
+
+EditPost.defaultProps = {};
+
+EditPost.propTypes = {};
+
+export default EditPost;
