d9bda63424bbb903a50ef16de7f56072cbbf7163
JP Erasmus <jperasmus11@gmail.com>
2017-12-27 20:56:29 +0200

    chore: Replace manual `logger` redux middleware with official package

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './reducers';
-
-const logger = store => next => action => {
-  console.group(action.type);
-  console.info('dispatching', action);
-  const result = next(action);
-  console.log('next state', store.getState());
-  console.groupEnd(action.type);
-  return result;
-};
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
